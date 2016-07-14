7eddac19b8c6df6313942dc678f7f40e3d9d12e7
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-07-14 23:04:14 +0200

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'nolds',
+  packages = ['nolds'],
+  version = '0.1',
+  description = 'Nonlinear measures for dynamical systems (based on one-dimensional timeseries)',
+  author = 'Christopher Schölzel',
+  author_email = 'christopher.schoelzel@gmx.net',
+  #url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  #download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+  keywords = [
+    'nonlinear', 'dynamical system', 'chaos', 
+    'lyapunov', 'hurst', 'hurst exponent', 'rescaled range', 'DFA', 
+    'detrended fluctuation analysis', 'sample entropy', 'correlation dimension'],
+  classifiers = [],
+)
