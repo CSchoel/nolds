a1fcf55a836caaf0f123b1e0b09aaf4c03a21f2d
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-08-01 20:14:15 +0200

    bugfix: we need to use nolds.measures instead of just measures when importing in package structure (+ some typos)

--- nolds/__init__.py
+++ nolds/__init__.py
@@ -1,1 +1,1 @@
-from measures import lyap_r, layp_e, sampen, hurs_rs, corr_dim, dfa, fbm, binary_n, logarithmic_n, logarithmic_r
+from nolds.measures import lyap_r, lyap_e, sampen, hurst_rs, corr_dim, dfa, fbm, binary_n, logarithmic_n, logarithmic_r
