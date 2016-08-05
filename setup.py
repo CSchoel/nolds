76986b33ef50692a6ba6390005b01a18c8ff780d
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-08-05 17:09:49 +0200

    updates version number

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'nolds',
   packages = ['nolds'],
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'Nonlinear measures for dynamical systems (based on one-dimensional time series)',
   author = 'Christopher Schölzel',
   author_email = 'christopher.schoelzel@gmx.net',
   url = 'https://github.com/CSchoel/nolds',
-  download_url = 'https://github.com/CSchoel/nolds/tarball/0.1.0',
+  download_url = 'https://github.com/CSchoel/nolds/tarball/0.1.1',
   keywords = [
     'nonlinear', 'dynamical system', 'chaos', 
     'lyapunov', 'hurst', 'hurst exponent', 'rescaled range', 'DFA', 
