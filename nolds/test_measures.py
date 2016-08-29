564aab39083128c6cc448627ebda210f2190e66b
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-08-29 17:53:00 +0200

    moves plotting function out of unittests

--- nolds/test_measures.py
+++ nolds/test_measures.py
@@ -70,51 +70,51 @@ class TestNoldsUtility(unittest.TestCase):
 		self.assertSequenceEqual(x, [4, 6.04, 9.1204])
 
 class TestNoldsLyap(unittest.TestCase):
-	def test_lyap(self):
-		# TODO maybe this should not be a unittest but an example?
-		import matplotlib.pyplot as plt # local import to avoid dependency for non-debug use
-		rvalues = np.arange(2, 4, 0.01)
-		lambdas = []
-		lambdas_est = []
-		lambdas_est2 = []
-		maps = []
-		logistic = lambda x : r * x * (1 - x)
-		for r in rvalues:
-			x = 0.1
-			result = []
-			full_data = [x]
-			# ignore first 100 values for bifurcation plot
-			for t in range(100):
-				x = logistic(x)
-				tmp = abs(r-2*r*x)
-				dx = np.nan if tmp <= 0 else np.log(tmp)
-				result.append(dx)
-				full_data.append(x)
-			lambdas.append(np.mean(result))
-			for t in range(20):
-				x = logistic(x)
-				maps.append(x)
-				full_data.append(x)
-			le = nolds.lyap_e(np.array(full_data), emb_dim=6, matrix_dim=2)
-			lambdas_est.append(np.max(le))
-			lambdas_est2.append(nolds.lyap_r(np.array(full_data), emb_dim=6, lag=2, min_tsep=10, trajectory_len=20))
-		plt.plot(rvalues, lambdas, "b-", label="true lyap. exponent")
-		plt.plot(rvalues, lambdas_est, color="#00AAAA", label="estimation using lyap_e")
-		plt.plot(rvalues, lambdas_est2, color="#AA00AA", label="estimation using lyap_r")
-		plt.plot(rvalues, np.zeros(len(rvalues)), "g--")
-		xvals = np.repeat(rvalues, 20)
-		plt.plot(xvals, maps, "ro", alpha=0.1, label="bifurcation plot")
-		plt.ylim((-2,2))
-		plt.xlabel("r")
-		plt.ylabel("lyap. exp / logistic(x, r)")
-		plt.legend(loc="best")
-		plt.show()
-		# TODO add some asserts
 	def test_lyap_fbm(self):
 		# TODO use logistic function as example
 		data = nolds.fbm(1000, H=0.3)
 		le = nolds.lyap_e(data, emb_dim=7, matrix_dim=3)
 		self.assertGreater(np.max(le), 0)
+
+def plot_lyap():
+	# TODO this should be moved into an example file
+	import matplotlib.pyplot as plt # local import to avoid dependency for non-debug use
+	rvalues = np.arange(2, 4, 0.01)
+	lambdas = []
+	lambdas_est = []
+	lambdas_est2 = []
+	maps = []
+	logistic = lambda x : r * x * (1 - x)
+	for r in rvalues:
+		x = 0.1
+		result = []
+		full_data = [x]
+		# ignore first 100 values for bifurcation plot
+		for t in range(100):
+			x = logistic(x)
+			tmp = abs(r-2*r*x)
+			dx = np.nan if tmp <= 0 else np.log(tmp)
+			result.append(dx)
+			full_data.append(x)
+		lambdas.append(np.mean(result))
+		for t in range(20):
+			x = logistic(x)
+			maps.append(x)
+			full_data.append(x)
+		le = nolds.lyap_e(np.array(full_data), emb_dim=6, matrix_dim=2)
+		lambdas_est.append(np.max(le))
+		lambdas_est2.append(nolds.lyap_r(np.array(full_data), emb_dim=6, lag=2, min_tsep=10, trajectory_len=20))
+	plt.plot(rvalues, lambdas, "b-", label="true lyap. exponent")
+	plt.plot(rvalues, lambdas_est, color="#00AAAA", label="estimation using lyap_e")
+	plt.plot(rvalues, lambdas_est2, color="#AA00AA", label="estimation using lyap_r")
+	plt.plot(rvalues, np.zeros(len(rvalues)), "g--")
+	xvals = np.repeat(rvalues, 20)
+	plt.plot(xvals, maps, "ro", alpha=0.1, label="bifurcation plot")
+	plt.ylim((-2,2))
+	plt.xlabel("r")
+	plt.ylabel("lyap. exp / logistic(x, r)")
+	plt.legend(loc="best")
+	plt.show()
 
 def test_lyap2():
 	#test_lyap()
