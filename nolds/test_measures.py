158163fea0ba1fbc86c9ddc35345568b953b2028
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-09-22 22:12:14 +0200

    moves plot_lyap and profiling to examples module

--- nolds/test_measures.py
+++ nolds/test_measures.py
@@ -101,46 +101,6 @@ class TestNoldsDFA(unittest.TestCase):
 			#self.assertAlmostEqual(he, h + 1, delta=0.1)
 			# TODO why is this not working?
 
-def plot_lyap():
-	# TODO this should be moved into an example file
-	import matplotlib.pyplot as plt # local import to avoid dependency for non-debug use
-	rvalues = np.arange(2, 4, 0.01)
-	lambdas = []
-	lambdas_est = []
-	lambdas_est2 = []
-	maps = []
-	logistic = lambda x : r * x * (1 - x)
-	for r in rvalues:
-		x = 0.1
-		result = []
-		full_data = [x]
-		# ignore first 100 values for bifurcation plot
-		for t in range(100):
-			x = logistic(x)
-			tmp = abs(r-2*r*x)
-			dx = np.nan if tmp <= 0 else np.log(tmp)
-			result.append(dx)
-			full_data.append(x)
-		lambdas.append(np.mean(result))
-		for t in range(20):
-			x = logistic(x)
-			maps.append(x)
-			full_data.append(x)
-		le = nolds.lyap_e(np.array(full_data), emb_dim=6, matrix_dim=2)
-		lambdas_est.append(np.max(le))
-		lambdas_est2.append(nolds.lyap_r(np.array(full_data), emb_dim=6, lag=2, min_tsep=10, trajectory_len=20))
-	plt.plot(rvalues, lambdas, "b-", label="true lyap. exponent")
-	plt.plot(rvalues, lambdas_est, color="#00AAAA", label="estimation using lyap_e")
-	plt.plot(rvalues, lambdas_est2, color="#AA00AA", label="estimation using lyap_r")
-	plt.plot(rvalues, np.zeros(len(rvalues)), "g--")
-	xvals = np.repeat(rvalues, 20)
-	plt.plot(xvals, maps, "ro", alpha=0.1, label="bifurcation plot")
-	plt.ylim((-2,2))
-	plt.xlabel("r")
-	plt.ylabel("lyap. exp / logistic(x, r)")
-	plt.legend(loc="best")
-	plt.show()
-
 def test_lyap2():
 	#test_lyap()
 	data = [1,2,4,5,6,6,1,5,1,2,4,5,6,6,1,5,1,2,4,5,6,6,1,5]
@@ -173,13 +133,6 @@ def test_dfa():
 	plt.plot(data)
 	plt.show()
 	print(nolds.dfa(data))
-
-def profiling():
-	import cProfile
-	n = 100000
-	data = np.cumsum(np.random.random(n)-0.5)
-	cProfile.runctx('lyap_e(data)',{'lyap_e': lyap_e},{'data': data})
-
 
 if __name__ == "__main__":
 	unittest.main()
