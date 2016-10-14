3e9e8f71d71d80285c25176c3dd4ad71d0cafcfd
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-10-14 17:11:29 +0200

    adds version variable for setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
+version = '0.2.0'
 setup(
     name='nolds',
     packages=['nolds'],
-    version='0.2.0',
+    version=version,
     description='Nonlinear measures for dynamical systems '
                 + '(based on one-dimensional time series)',
     author='Christopher Schölzel',
     author_email='christopher.schoelzel@gmx.net',
     url='https://github.com/CSchoel/nolds',
-    download_url='https://github.com/CSchoel/nolds/tarball/0.2.0',
+    download_url='https://github.com/CSchoel/nolds/tarball/' + version,
     keywords=[
         'nonlinear', 'dynamical system', 'chaos',
         'lyapunov', 'hurst', 'hurst exponent', 'rescaled range', 'DFA',
