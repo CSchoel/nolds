723610386a3600167fbdc0d713376b74947dc6c7
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-11-18 17:57:37 +0100

    removes set_fitting_mode, FIT_POLY and FIT_RANSAC from imports

--- nolds/__init__.py
+++ nolds/__init__.py
@@ -1,3 +1,2 @@
 from .measures import lyap_r, lyap_e, sampen, hurst_rs, corr_dim, dfa, \
-    fbm, binary_n, logarithmic_n, logarithmic_r, set_fitting_mode, FIT_POLY, \
-    FIT_RANSAC
+    fbm, binary_n, logarithmic_n, logarithmic_r
