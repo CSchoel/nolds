ab534bc1c03ac59ec68979ec37c3da166f451bb0
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-11-18 15:57:59 +0100

    bugfix: setuptools cannot handle unicode_literal import

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
+                        print_function)
 from builtins import *
 from setuptools import setup, Command
 import glob
