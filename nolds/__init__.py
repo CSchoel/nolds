a2a2d0ab1695297c24634f5462f5ca688d2584e8
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2017-11-21 14:59:32 +0100

    adds qrandom and load_qrandom to nolds main package

--- nolds/__init__.py
+++ nolds/__init__.py
@@ -1,3 +1,4 @@
 from .measures import lyap_r, lyap_e, sampen, hurst_rs, corr_dim, dfa, \
     binary_n, logarithmic_n, logarithmic_r, expected_h
-from .datasets import brown72, tent_map, logistic_map, fbm, fgn
+from .datasets import brown72, tent_map, logistic_map, fbm, fgn, qrandom, \
+    load_qrandom
