8e283a36f2a9936bb8ca8984e1b0a2e6fef487bf
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2018-10-23 13:50:41 +0200

    ensures that data length cannot be lower than 1 in test

--- nolds/test_measures.py
+++ nolds/test_measures.py
@@ -179,7 +179,7 @@ class TestNoldsLyap(unittest.TestCase):
       kwargs["emb_dim"] = np.random.randint(1,4) \
                         * (kwargs["matrix_dim"] - 1) + 1
       min_len = nolds.lyap_e_len(**kwargs)
-      for i in reversed(range(min_len-5,min_len+5)):
+      for i in reversed(range(max(1,min_len-5),min_len+5)):
         data = np.random.random(i)
         if i < min_len:
           ## too few data points => execution should fail
