7fefb77dfaf5ead57204962f97251095ac3f5dcd
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-02-15 22:59:03 +0100

    bugfix: we need to divide instead of subtract

--- nolds/measures.py
+++ nolds/measures.py
@@ -1302,7 +1302,7 @@ def hurst_multifractal(data, qvals=[1], delta_d=1, dists=range(1, 20)):
       np.log(dists * delta_d),
       np.log(corrvals[:, qi]),
       1
-    )[0] - qvals[qi]
+    )[0] / qvals[qi]
     for qi in range(len(qvals))
   ]
   return H
