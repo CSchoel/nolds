f97b8fff912a018be0ca81637b028f8164b5970e
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-02-17 23:54:33 +0100

    fixes formatting of genhurst

--- nolds/measures.py
+++ nolds/measures.py
@@ -1366,7 +1366,7 @@ def mfhurst_b(data, qvals=[1], dists=range(1, 20),
   return H
 
 
-def genhurst(S,q):
+def genhurst(S, q):
     """
     ####################################
     # Calculates the generalized Hurst exponent H(q) from the scaling
@@ -1393,42 +1393,47 @@ def genhurst(S,q):
     ####################################
     ##   written in Matlab : Tomaso Aste, 30/01/2013 ##
     ##   translated to Python (3.6) : Peter Rupprecht, p.t.r.rupprecht (AT) gmail.com, 25/05/2017 ##
+    ##   formatting and datatype fixes : Christopher Schölzel, 17/02/2019 ##
     """
-    L=len(S)       
+    L = len(S)
     if L < 100:
         warnings.warn('Data series very short!')
-       
-    H = np.zeros((len(range(5,20)),1))
+    H = np.zeros((len(range(5, 20)), 1))
     k = 0
-    
-    for Tmax in range(5,20):
-        
-        x = np.arange(1,Tmax+1,1)
-        mcord = np.zeros((Tmax,1))
-        
-        for tt in range(1,Tmax+1):
-            dV = S[np.arange(tt,L,tt)] - S[np.arange(tt,L,tt)-tt]
-            VV = S[np.arange(tt,L+tt,tt)-tt]
+
+    for Tmax in range(5, 20):
+
+        x = np.arange(1, Tmax+1, 1)
+        mcord = np.zeros((Tmax, 1))
+
+        for tt in range(1, Tmax+1):
+            dV = S[np.arange(tt, L, tt)] - S[np.arange(tt, L, tt)-tt]
+            VV = S[np.arange(tt, L+tt, tt)-tt]
             N = len(dV) + 1
-            X = np.arange(1,N+1, dtype=np.float64)
+            X = np.arange(1, N+1, dtype=np.float64)
             Y = VV
             mx = np.sum(X)/N
             SSxx = np.sum(X**2) - N*mx**2
             my = np.sum(Y)/N
-            SSxy = np.sum( np.multiply(X,Y))  - N*mx*my
+            SSxy = np.sum(np.multiply(X, Y)) - N*mx*my
             cc1 = SSxy/SSxx
             cc2 = my - cc1*mx
             ddVd = dV - cc1
-            VVVd = VV - np.multiply(cc1,np.arange(1,N+1, dtype=np.float64)) - cc2
-            mcord[tt-1] = np.mean( np.abs(ddVd)**q )/np.mean( np.abs(VVVd)**q )
+            VVVd = VV - np.multiply(cc1, np.arange(1, N+1, dtype=np.float64)) \
+                      - cc2
+            mcord[tt-1] = np.mean(np.abs(ddVd)**q)/np.mean(np.abs(VVVd)**q)
         mx = np.mean(np.log10(x))
-        SSxx = np.sum( np.log10(x)**2) - Tmax*mx**2
+        SSxx = np.sum(np.log10(x)**2) - Tmax*mx**2
         my = np.mean(np.log10(mcord))
-        SSxy = np.sum( np.multiply(np.log10(x),np.transpose(np.log10(mcord)))) - Tmax*mx*my
+        SSxy = np.sum(
+          np.multiply(
+            np.log10(x), np.transpose(np.log10(mcord))
+          )
+        ) - Tmax*mx*my
         H[k] = SSxy/SSxx
         k = k + 1
     mH = np.mean(H)/q
-    
+
     return mH
 
 
