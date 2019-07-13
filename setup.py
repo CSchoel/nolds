35f622d5fa15542bfcd8d587d4c7c34bfe092d87
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-07-13 18:39:52 +0200

    removes unnecessary future imports in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import (absolute_import, division,
-                        print_function)
-from builtins import *
 from setuptools import setup, Command
 import glob
 import shutil
