666cb4c05a2d06dc8f530ae8c52f9db0ad54699e
Christopher Schölzel <christopher.schoelzel@gmx.net>
2019-07-14 11:08:25 +0200

    makes bultin imports explicit

--- nolds/measures.py
+++ nolds/measures.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
-from builtins import *
+from builtins import (
+  bytes, dict, int, list, object, range, str, ascii, chr, hex, input, next,
+  oct, open, pow, round, super, filter, map, zip
+)
 import numpy as np
 import warnings
 import math
