dccd4121a019d82dca0c74c85ee34d71c3baf39c
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-05-08 11:53:40 +0200

    adds rowwise_chebyshev and rowwise_euclidean to exports

--- nolds/__init__.py
+++ nolds/__init__.py
@@ -1,5 +1,5 @@
 from .measures import lyap_r, lyap_e, sampen, hurst_rs, corr_dim, dfa, \
     binary_n, logarithmic_n, logarithmic_r, expected_h, logmid_n, expected_rs, \
-    lyap_r_len, lyap_e_len
+    lyap_r_len, lyap_e_len, rowwise_chebyshev, rowwise_euclidean
 from .datasets import brown72, tent_map, logistic_map, fbm, fgn, qrandom, \
     load_qrandom
