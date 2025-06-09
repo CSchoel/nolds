db45508bcdff2b26622efedca5a0a9563dd8c0d5
Christopher Schölzel <christopher.schoelzel@mailbox.org>
2025-06-09 19:23:10 +0200

    bumps version to 0.6.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class CleanCommand(Command):
 
 with io.open("README.rst", "r", encoding="utf-8") as f:
     readme = f.read()
-version = '0.6.1'
+version = '0.6.2'
 setup(
     name='nolds',
     packages=['nolds'],
