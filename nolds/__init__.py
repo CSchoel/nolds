2e2ff699e1db55f78af8748235a503c1743565b3
Christopher Schölzel <christopher.schoelzel@mailbox.org>
2025-06-11 22:08:25 +0200

    fixes remaining linter errors

--- nolds/__init__.py
+++ nolds/__init__.py
@@ -1,3 +1,5 @@
+"""Main module for nolds, containing all important user-facing API elements."""
+
 from .datasets import (
     barabasi1991_fractal,
     brown72,
@@ -29,3 +31,33 @@ from .measures import (
     rowwise_euclidean,
     sampen,
 )
+
+__all__ = [
+    "barabasi1991_fractal",
+    "binary_n",
+    "brown72",
+    "corr_dim",
+    "dfa",
+    "expected_h",
+    "expected_rs",
+    "fbm",
+    "fgn",
+    "hurst_rs",
+    "load_financial",
+    "load_qrandom",
+    "logarithmic_n",
+    "logarithmic_r",
+    "logistic_map",
+    "logmid_n",
+    "lyap_e",
+    "lyap_e_len",
+    "lyap_r",
+    "lyap_r_len",
+    "mfhurst_b",
+    "mfhurst_dm",
+    "qrandom",
+    "rowwise_chebyshev",
+    "rowwise_euclidean",
+    "sampen",
+    "tent_map",
+]
